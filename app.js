757065ff083cd7f2bb189886813783016c2b2f72
Nan Davenport <nannigan@nannigans.com>
2015-05-14 13:43:44 -0700

    attempt at get by tags

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 var app= angular.module('app', [])
 .controller('myController', function($scope, $http){
 		$scope.getPics = function(data) {
-		var url = "https://api.instagram.com/v1/tags/nofilter/media/recent";
-		var request = {
-			client_id:'40f58b3815c04090b0918afa12312859',
-			data: data.images
-
-		};
-	$http({
-		  method: 'JSONP',
-			url: url,
-			params: request
-		})
-	.success(function(response){
-     $scope.data = response;
-    // var weFound = $scope.function(elem){
-    // 	return elem.css('display', 'inline-block');};
-     console.log($scope.data);
-	  })
-	.error(function(){
-		 alert('uhoh');
-	  });
+				var url = "https://api.instagram.com/v1/tags/" + data + "/media/recent";
+				var request = {
+					client_id:'40f58b3815c04090b0918afa12312859',
+					//data: data.images // goes to error alert console.logs 
+					//Uncaught SyntaxError: recent?client_id=40f58b3815c04090b0918afa12312859:1 
+				};
+			$http({
+				  method: 'JSONP',
+					url: url,
+					params: request,
+					callback: 'JSON_CALLBACK'
+				})
+			.success(function(response){
+		     $scope.data = response;
+		  
+		     console.log($scope.data);
+			  })
+			.error(function(){
+				 alert('uhoh');
+			  });
   }
 });//end controller
 
